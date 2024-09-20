83fce81190c31d222a78948577cb9a3ecc553dbf
Madhav <Madhavampire@gmail.com>
2024-09-21 03:06:59 +0530

    1

---
+++ blogs/1/blog.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    async function loadTitle() {
+        try {
+            const response = await fetch('meta.json');
+            if (!response.ok) throw new Error('Error fetching blog metadata.');
+            const blogData = await response.json();
+            document.title = blogData.title;
+        } catch (error) {
+            console.error('Error loading title:', error);
+        }
+    }
+
+    async function loadBlogContent() {
+        try {
+            const response = await fetch('blog.md');
+            if (!response.ok) throw new Error('Error fetching blog.md');
+
+            const markdownContent = await response.text();
+            document.getElementById('blog-full').innerHTML = marked.parse(markdownContent);
+        } catch (error) {
+            document.getElementById('blog-full').innerHTML = '<p>Error loading the blog content.</p>';
+            console.error('Error loading blog content:', error);
+        }
+    }
+
+    loadTitle();
+    loadBlogContent();
+});
